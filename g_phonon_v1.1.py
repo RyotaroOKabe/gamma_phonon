46f000ff1deb250343ab2fcb32e5a04b9bf1c33e
RyotaroOkabe <rytrokb@gmail.com>
2022-08-18 08:53:49 -0400

    g-ph

---
+++ g_phonon_v1.1.py
@@ -0,0 +1,306 @@
+#%%
+# model
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch_geometric as tg
+import torch_scatter
+import e3nn
+from e3nn import o3
+from typing import Dict, Union
+
+# crystal structure data
+from ase import Atom, Atoms
+from ase.neighborlist import neighbor_list
+from ase.visualize.plot import plot_atoms
+
+# data pre-processing and visualization
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from IPython.display import HTML
+import pandas as pd
+
+# utilities
+import time
+from tqdm import tqdm
+from utils.utils_data import (load_data, train_valid_test_split, plot_example, plot_predictions, plot_partials,
+                              palette, colors, cmap)
+from utils.utils_model import Network, visualize_layers, train
+from utils.utils_plot import plotly_surface, plot_orbitals, get_middle_feats
+
+bar_format = '{l_bar}{bar:10}{r_bar}{bar:-10b}'
+default_dtype = torch.float64
+torch.set_default_dtype(default_dtype)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print('torch device:' , device)
+max_iter = 100
+pad_size = 39
+print('max_iter: ', max_iter)
+print("pad_size: ", pad_size)
+
+#%%
+# pad the gamma-phjonon data with zero, or cut the lengthy part.
+def tosize(a, n):
+    """_summary_
+
+    Args:
+        a (np.array): gamma-phonon 
+        n (int): length of the gamma phonon data after padding/cutting data.
+
+    Returns:
+        np.array: 
+    """
+    a = np.asarray(a)
+    x = np.zeros(n, dtype=a.dtype)
+    m = min(n, len(a))
+    x[:m] = a[:m]
+    return x
+
+
+#%%
+
+df = pd.read_pickle(f'data/df_struct_phonon.pkl')
+species = sorted(list(set(df['species'].sum())))
+from dataframe_v1 import tosize
+df['gph'] = df['g_phs'].map(lambda x: tosize(x, pad_size))
+
+#%%
+
+# plot an example structure
+i = 120 # structure index in dataframe
+
+struct = df.iloc[i]['structure']
+symbols = np.unique(list(struct.symbols))
+z = dict(zip(symbols, range(len(symbols))))
+
+fig, ax = plt.subplots(figsize=(6,5))
+norm = plt.Normalize(vmin=0, vmax=len(symbols)-1)
+color = [mpl.colors.to_hex(k) for k in cmap(norm([z[j] for j in list(struct.symbols)]))]
+plot_atoms(struct, ax, radii=0.25, colors=color, rotation=('30x,30y,0z'))
+
+ax.set_xlabel(r'$x_1\ (\AA)$')
+ax.set_ylabel(r'$x_2\ (\AA)$');
+fig.patch.set_facecolor('white')
+fig.suptitle(f"{df.iloc[i]['id']} / {df.iloc[i]['formula']}", fontsize=15)
+
+
+#%%
+# one-hot encoding atom type and mass
+type_encoding = {}
+specie_am = []
+for Z in tqdm(range(1, 119), bar_format=bar_format):
+    specie = Atom(Z)
+    type_encoding[specie.symbol] = Z
+    specie_am.append(specie.mass)
+
+type_onehot = torch.eye(len(type_encoding))
+am_onehot = torch.diag(torch.tensor(specie_am))
+
+#%%
+# build data
+def build_data(entry, type_encoding, type_onehot, r_max=5.):
+    symbols = list(entry.structure.symbols).copy()
+    positions = torch.from_numpy(entry.structure.positions.copy())
+    lattice = torch.from_numpy(entry.structure.cell.array.copy()).unsqueeze(0)
+
+    # edge_src and edge_dst are the indices of the central and neighboring atom, respectively
+    # edge_shift indicates whether the neighbors are in different images or copies of the unit cell
+    edge_src, edge_dst, edge_shift = neighbor_list("ijS", a=entry.structure, cutoff=r_max, self_interaction=True)
+    
+    # compute the relative distances and unit cell shifts from periodic boundaries
+    edge_batch = positions.new_zeros(positions.shape[0], dtype=torch.long)[torch.from_numpy(edge_src)]
+    edge_vec = (positions[torch.from_numpy(edge_dst)]
+                - positions[torch.from_numpy(edge_src)]
+                + torch.einsum('ni,nij->nj', torch.tensor(edge_shift, dtype=default_dtype), lattice[edge_batch]))
+
+    # compute edge lengths (rounded only for plotting purposes)
+    edge_len = np.around(edge_vec.norm(dim=1).numpy(), decimals=2)
+    
+    data = tg.data.Data(
+        pos=positions, lattice=lattice, symbol=symbols,
+        x=am_onehot[[type_encoding[specie] for specie in symbols]],   # atomic mass (node feature)
+        z=type_onehot[[type_encoding[specie] for specie in symbols]], # atom type (node attribute)
+        edge_index=torch.stack([torch.LongTensor(edge_src), torch.LongTensor(edge_dst)], dim=0),
+        edge_shift=torch.tensor(edge_shift, dtype=default_dtype),
+        edge_vec=edge_vec, edge_len=edge_len,
+        gph=torch.from_numpy(entry.gph).unsqueeze(0)
+    )
+    
+    return data
+
+r_max = 4. # cutoff radius
+df['data'] = df.progress_apply(lambda x: build_data(x, type_encoding, type_onehot, r_max), axis=1)
+
+
+#%%
+# train/valid/test split
+idx_train, idx_valid, idx_test = train_valid_test_split(df, species, valid_size=0.1, test_size=0.1, seed=12, plot=True)
+
+
+#%%
+
+with open('data/idx_train.txt', 'w') as f:
+    for line in idx_train:
+        f.write(str(line))
+        f.write('\n')
+        
+with open('data/idx_valid.txt', 'w') as f:
+    for line in idx_valid:
+        f.write(str(line))
+        f.write('\n')
+
+with open('data/idx_test.txt', 'w') as f:
+    for line in idx_test:
+        f.write(str(line))
+        f.write('\n')
+
+
+# load train/valid/test indices
+with open('data/idx_train.txt', 'r') as f: idx_train = [int(i.split('\n')[0]) for i in f.readlines()]
+with open('data/idx_valid.txt', 'r') as f: idx_valid = [int(i.split('\n')[0]) for i in f.readlines()]
+with open('data/idx_test.txt', 'r') as f: idx_test = [int(i.split('\n')[0]) for i in f.readlines()]
+
+# format dataloaders
+batch_size = 1
+dataloader_train = tg.loader.DataLoader(df.iloc[idx_train]['data'].values, batch_size=batch_size, shuffle=True)
+dataloader_valid = tg.loader.DataLoader(df.iloc[idx_valid]['data'].values, batch_size=batch_size)
+dataloader_test = tg.loader.DataLoader(df.iloc[idx_test]['data'].values, batch_size=batch_size)
+
+
+# calculate average number of neighbors
+def get_neighbors(df, idx):
+    n = []
+    for entry in df.iloc[idx].itertuples():
+        N = entry.data.pos.shape[0]
+        for i in range(N):
+            n.append(len((entry.data.edge_index[0] == i).nonzero()))
+    return np.array(n)
+
+n_train = get_neighbors(df, idx_train)
+n_valid = get_neighbors(df, idx_valid)
+n_test = get_neighbors(df, idx_test)
+
+fig, ax = plt.subplots(1,1, figsize=(5,4))
+b = 0.
+bins = 50
+for (d, c), n in zip(colors.items(), [n_train, n_valid, n_test]):
+    color = [int(c.lstrip('#')[i:i+2], 16)/255. for i in (0,2,4)]
+    y, bins, _, = ax.hist(n, bins=bins, fc=color+[0.7], ec=color, bottom=b, label=d)
+    b += y
+ax.set_xlabel('number of neighbors')
+ax.set_ylabel('number of examples')
+ax.legend(frameon=False)
+
+print('average number of neighbors (train/valid/test):', n_train.mean(), '/', n_valid.mean(), '/', n_test.mean())
+
+
+#%%
+
+class PeriodicNetwork(Network):
+    def __init__(self, in_dim, em_dim, **kwargs):            
+        # override the `reduce_output` keyword to instead perform an averge over atom contributions    
+        self.pool = False
+        if kwargs['reduce_output'] == True:
+            kwargs['reduce_output'] = False
+            self.pool = True
+            
+        super().__init__(**kwargs)
+
+        # embed the mass-weighted one-hot encoding
+        self.em = nn.Linear(in_dim, em_dim)
+
+    def forward(self, data: Union[tg.data.Data, Dict[str, torch.Tensor]]) -> torch.Tensor:
+        data.x = F.relu(self.em(data.x))
+        data.z = F.relu(self.em(data.z))
+        output = super().forward(data)
+        output = torch.relu(output)
+        
+        # if pool_nodes was set to True, use scatter_mean to aggregate
+        if self.pool == True:
+            output = torch_scatter.scatter_mean(output, data.batch, dim=0)  # take mean over atoms per example
+        
+        maxima, _ = torch.max(output, dim=1)
+        output = output.div(maxima.unsqueeze(1))
+        
+        return output
+
+
+#%%
+out_dim = len(df.iloc[0]['gph'])
+em_dim = 64  
+
+model = PeriodicNetwork(
+    in_dim=118,                            # dimension of one-hot encoding of atom type
+    em_dim=em_dim,                         # dimension of atom-type embedding
+    irreps_in=str(em_dim)+"x0e",           # em_dim scalars (L=0 and even parity) on each atom to represent atom type
+    irreps_out=str(out_dim)+"x0e",         # out_dim scalars (L=0 and even parity) to output
+    irreps_node_attr=str(em_dim)+"x0e",    # em_dim scalars (L=0 and even parity) on each atom to represent atom type
+    layers=2,                              # number of nonlinearities (number of convolutions = layers + 1)
+    mul=32,                                # multiplicity of irreducible representations
+    lmax=1,                                # maximum order of spherical harmonics
+    max_radius=r_max,                      # cutoff radius for convolution
+    num_neighbors=n_train.mean(),          # scaling factor based on the typical number of neighbors
+    reduce_output=True                     # whether or not to aggregate features of all atoms at the end
+)
+
+print(model)
+
+#%%
+# visualize tensor products of the model
+visualize_layers(model)
+
+#%%
+opt = torch.optim.AdamW(model.parameters(), lr=0.005, weight_decay=0.05)
+scheduler = torch.optim.lr_scheduler.ExponentialLR(opt, gamma=0.96)
+
+loss_fn = torch.nn.MSELoss()
+loss_fn_mae = torch.nn.L1Loss()
+
+
+run_name = 'model_' + time.strftime("%y%m%d", time.localtime())
+print(run_name)
+
+#%%
+model.pool = True
+train(model, opt, dataloader_train, dataloader_valid, loss_fn, loss_fn_mae, run_name,
+      max_iter=max_iter, scheduler=scheduler, device=device)
+
+
+#%%
+# load pre-trained model and plot its training history
+run_name = run_name
+print(run_name)
+
+history = torch.load(run_name + '.torch', map_location=device)['history']
+steps = [d['step'] + 1 for d in history]
+loss_train = [d['train']['loss'] for d in history]
+loss_valid = [d['valid']['loss'] for d in history]
+
+fig, ax = plt.subplots(figsize=(6,5))
+ax.plot(steps, loss_train, 'o-', label="Training", color=colors['train'])
+ax.plot(steps, loss_valid, 'o-', label="Validation", color=colors['valid'])
+ax.set_xlabel('epochs')
+ax.set_ylabel('loss')
+ax.legend(frameon=False);
+fig.savefig(f'savefig/{run_name}_ep{max_iter}.png')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
